555b545f18d46b5caa9863df2bd61ac0aba532d4
1Canis-Lupus1 <shubhamchoudhary081997@gmail.com>
2020-10-12 20:46:22 +0530

    Pushing into Children array left

--- src/components/FolderForm.jsx
+++ src/components/FolderForm.jsx
@@ -2,36 +2,38 @@ import React, { useState, useContext } from 'react'
 import { FolderListContext } from '../contexts/FolderListContext'
 
 const FolderForm = () => {
-  const { folders,addFolder } = useContext(FolderListContext)
-  const [title, setTitle] = useState('')
+  const { folders, addFolder } = useContext(FolderListContext)
+  const [title, setTitle] = useState('');
 
   const handleSubmit = e => {
     e.preventDefault();
     addFolder(title);
     // console.log(title);
     setTitle('');
-
   }
 
   const handleChange = e => {
     setTitle(e.target.value)
   }
 
-  const handleClick=(e)=>{
+  const handleClick = (e) => {
     // console.log("Folder ID",e.target.getAttribute("folder-index"))
-    const index=e.target.getAttribute("data-index");
-    const folderIndex=e.target.getAttribute("folder-index");
+    const index = e.target.getAttribute("data-index");
+    const folderIndex = e.target.getAttribute("folder-index");
+    // console.log(typeof(folders))
+    let dummyFolders=[...folders]
+    let parent=dummyFolders.forEach(item=>{
+      //Checking the item which matches the selected list
+      if (item.id === folderIndex) {
+        console.log("Matched");
+        console.log(item.title);
+        item.children.push("Sample")
+        console.log("Dummy",item.children)
+      }
 
-    if(folders[index].id === folderIndex){
-      console.log("Matched");
-      addFolder(title);
-      setTitle('');
-      // console.log("Children: ",folders[index].children)
-    }
-    console.log("Clicked Folder ID:",e.target);
-    console.log(title)
+    })
 
-    
+    // console.log("Clicked Folder ID:",e.target);
   }
 
   return (
@@ -46,24 +48,24 @@ const FolderForm = () => {
       />
       <button type="submit" className="btn add-task-btn">Add Folder</button>
       <div>
-      {folders.length ? (
-        <ul className="list">
-          {folders.map((folders,index) => {
-            // console.log("Folder ID:",folders.id)
-            // return <Folder folders={folders} key={index} />;
-            return (
-              <div>
-                <li className="list-item" data-index={index} folder-index={folders.id} key={folders.id} onClick={handleClick}>
-                  {folders.title}
-                </li>
-              </div>
-            )
-          })}
-        </ul>
-      ) : (
-          <div className="no-tasks">Contains No Folder</div>
-        )}
-    </div>
+        {folders.length ? (
+          <ul className="list">
+            {folders.map((folders, index) => {
+              // console.log("Folder ID:",folders.id)
+              // return <Folder folders={folders} key={index} />;
+              return (
+                <div>
+                  <li className="list-item" data-index={index} folder-index={folders.id} key={folders.id} onClick={handleClick}>
+                    {folders.title}
+                  </li>
+                </div>
+              )
+            })}
+          </ul>
+        ) : (
+            <div className="no-tasks">Contains No Folder</div>
+          )}
+      </div>
     </form>
   )
 }
